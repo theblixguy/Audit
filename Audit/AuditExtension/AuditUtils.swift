86a0ead0ebc210c7db025da83aceedd38b34722e
Suyash Srijan <suyashsrijan@outlook.com>
2018-04-14 00:55:41 +0100

    Remove SwiftyJSON dependency and use JSONDecoder
    Changes:
    
    1. Remove SwiftyJSON dependency and use the native JSONDecoder() for JSON parsing.
    
    2. Replace OperationQueue with DispatchQueue as we don't really need the overhead of an OperationQueue for simple JSON parsing.
    
    3. Fix a small typo in "sources.json".

--- Audit/AuditExtension/AuditUtils.swift
+++ Audit/AuditExtension/AuditUtils.swift
@@ -8,23 +8,43 @@
 
 import Foundation
 import UIKit
-import SwiftyJSON
 
 class AuditUtils {
     
+    struct AuditItem: Decodable {
+        
+        enum CodingKeys: String, CodingKey {
+            case type = "type"
+            case secondType = "2nd type"
+            case thirdType = "3rd type"
+            case notes = "Source Notes (things to know?)"
+        }
+        
+        let type: AuditType
+        let secondType: AuditType
+        let thirdType: AuditType
+        let notes: String
+        
+        init(from decoder: Decoder) throws {
+            let con = try! decoder.container(keyedBy: CodingKeys.self)
+            type = try! AuditType(value: con.decode(String.self, forKey: AuditUtils.AuditItem.CodingKeys.type))
+            
+            secondType = try! AuditType(value: con.decode(String.self, forKey: AuditUtils.AuditItem.CodingKeys.secondType))
+            
+            thirdType = try! AuditType(value: con.decode(String.self, forKey: AuditUtils.AuditItem.CodingKeys.thirdType))
+            
+            notes = try! con.decode(String.self, forKey: AuditUtils.AuditItem.CodingKeys.notes)
+        }
+    }
+    
     static func getAuditType(host: String, completion: @escaping (AuditType)->()) {
-        let queue: OperationQueue = OperationQueue()
-        queue.addOperation {
+        DispatchQueue.global(qos: .userInitiated).async {
             let sourcesData = NSDataAsset(name: "sources", bundle: Bundle.main)
-            let json = try? JSON(data: sourcesData!.data)
-            if let type: String = json![host]["type"].string {
-                OperationQueue.main.addOperation({
-                    completion(AuditType(value: type))
-                })
+            let json = try! JSONDecoder().decode([String: AuditItem].self, from: sourcesData!.data)
+            if let auditItem = json.filter({$0.key == host}).first {
+                completion(auditItem.value.type)
             } else {
-                OperationQueue.main.addOperation({
-                    completion(AuditType.Unknown)
-                })
+                completion(AuditType.Unknown)
             }
         }
     }
--- Audit/Pods/Target Support Files/Pods-Audit/Pods-Audit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Audit : NSObject
-@end
-@implementation PodsDummy_Pods_Audit
-@end
